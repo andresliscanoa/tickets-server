4f7ed8a6e59db333a4fa1678983bc3b7a98ca4ae
andresliscanoa <andresliscanoa@gmail.com>
2022-05-04 22:16:54 -0500

    Rate Limit

--- src/factories/express.js
+++ src/factories/express.js
@@ -3,7 +3,9 @@ const express = require( 'express' )
 const cors = require( 'cors' )
 const helmet = require( 'helmet' )
 const morgan = require( 'morgan' )
+const rateLimit = require( 'express-rate-limit' )
 const { requestInfo } = require("../guards/requestInfo");
+const fs = require("fs");
 
 
 
@@ -21,6 +23,12 @@ app.use( function ( req, res, next ) {
     res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' )
     next()
 } )
+const apiLimiter = rateLimit( {
+    windowMs: 30 * 60 * 1000, //Abrimos una ventana de treinta minutos
+    max     : process.env.RATE_LIMIT, // Limitamos las peticiones a las API
+    message : 'Demasiadas peticiones desde esta misma IP, intente nuevamente màs tarde',
+} )
+app.use( apiLimiter )
 
 
 module.exports = app
